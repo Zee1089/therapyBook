9846a58ea6aa79dddda75ba778de1898b0abb621
Zee <zeeboo696@gmail.com>
2024-07-10 15:51:21 -0400

    Build the Trigger Schema

--- models/user.js
+++ models/user.js
@@ -9,8 +9,50 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  triggers: [triggerSchema],
 });
 
 const User = mongoose.model('User', userSchema);
+
+const mongoose = require('mongoose');
+
+const triggerSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+
+  people: {
+    type: Boolean,
+    required: true,
+  },
+
+  reaction: {
+    type: String,
+    required: true, 
+  },
+
+  rectification: {
+    type: String,
+    required: true,
+
+  },
+  notes: {
+    type: String,
+  },
+
+  status : {
+    type: String,
+    enum: ['succeded', 'recovered', 'working on it', 'overcomed', 'be careful next time']
+
+  }, 
+
+})
+
+
 
 module.exports = User;
