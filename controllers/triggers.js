acac0e13e92b68cf1594a91b2a286f35dbdf77e8
Zee <zeeboo696@gmail.com>
2024-07-10 16:52:36 -0400

    Build Create Functionality

--- controllers/triggers.js
+++ controllers/triggers.js
@@ -17,5 +17,20 @@ router.get('/new', async (req, res) => {
     res.render('triggers/new.ejs');
 });
 
+router.post('/', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      currentUser.triggers.push(req.body);
+      // Save changes to the user
+      await currentUser.save();
+      res.redirect(`/users/${currentUser._id}/triggers`);
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/')
+    }
+  });
+  
 module.exports = router;
 
