18540298dce91dd4a6b4705cd3d972473a312c21
Zee <zeeboo696@gmail.com>
2024-07-10 16:28:04 -0400

    Add Route Middleware

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+
 const morgan = require('morgan');
 const session = require('express-session');
+
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 
 const authController = require('./controllers/auth.js');
 //import the triggers controller into server.js
@@ -30,17 +35,23 @@ app.use(
   })
 );
 
+//PassUserToView should be included before all our routes, including our homepage, just
+app.use(passUserToView);
+
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+  if (req.session.user) {
+    res.redirect(`/users/${req.session.user._id}/triggers`);
+  } else {
+    res.render('index.ejs');
+  }
 });
 
 
 
 app.use('/auth', authController);
+app.use(isSignedIn);
 //link your controller to a specific route in server.js, incoming requests to /users/applications will be handeled by our triggers controller. 
-app.use('/users/triggers', triggersController);
+app.use('/users/:userId/triggers', triggersController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
