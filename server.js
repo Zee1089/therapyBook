029411562d7d888967875dabf5436b29f8d51f1a
Zee <zeeboo696@gmail.com>
2024-07-11 17:13:15 -0400

    Styling the Homepage

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const path = require('path')
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-
 const morgan = require('morgan');
 const session = require('express-session');
 
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
-
-
 const authController = require('./controllers/auth.js');
-
 const triggersController = require('./controllers/triggers.js');
 
-const path = require('path')
 const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 app.use(morgan('dev'));
-
-
-app.use(express.static(path.join(__dirname, ‘public’)));
-
 
 app.use(
   session({
@@ -51,8 +44,6 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
   }
 });
-
-
 
 app.use('/auth', authController);
 app.use(isSignedIn);
